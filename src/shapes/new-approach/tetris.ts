1ac6b6062e75bea65b951b092629c5bdffd8150a
Malik Elbadri <MalikCura@users.noreply.github.com>
2023-08-20 22:54:43 +0300

    Fixed movement

--- src/shapes/new-approach/tetris.ts
+++ src/shapes/new-approach/tetris.ts
@@ -87,9 +87,11 @@ export default class Tetris {
   private clearLastPosition() {
     for (let i = this.shape.start.row; i <= this.shape.end.row; i++) {
       for (let j = this.shape.start.col; j <= this.shape.end.col; j++) {
-        const ceil = this.rows[i].ceils[j];
-        ceil.active = false;
-        ceil.e.setAttribute("data-active", "false");
+        try {
+          const ceil = this.rows[i].ceils[j];
+          ceil.active = false;
+          ceil.e.setAttribute("data-active", "false");
+        } catch (error) {}
       }
     }
   }
@@ -100,6 +102,9 @@ export default class Tetris {
       for (let i = this.shape.start.row; i < this.shape.end.row; i++) {
         if (this.rows[i].ceils[this.shape.end.col + 1].isActive()) return;
       }
+    if (this.rows[this.shape.end.row + 1].ceils[this.shape.end.col].isActive())
+      return;
+
     this.clearLastPosition();
     this.shape.start.col++;
     this.shape.end.col++;
@@ -112,6 +117,8 @@ export default class Tetris {
       for (let i = this.shape.start.row; i < this.shape.end.row; i++) {
         if (this.rows[i].ceils[this.shape.start.col - 1].isActive()) return;
       }
+    if (this.rows[this.shape.end.row + 1].ceils[this.shape.end.col].isActive())
+      return;
 
     this.clearLastPosition();
     this.shape.start.col--;
@@ -120,15 +127,25 @@ export default class Tetris {
     this.updateLastPosition();
   }
 
+  public down() {
+    this.clearLastPosition();
+    this.shape.start.row++;
+    this.shape.end.row++;
+  }
+
   public start() {
     this.addShape().update();
     this.updateLastPosition();
 
     const ii = setInterval(() => {
-      if (this.shape.start.row === this.shape.lastPosition.start.row) {
+      if (
+        this.shape.start.row === this.shape.lastPosition.start.row ||
+        this.shape.end.row >= this.ROWS
+      ) {
         this.addShape();
         this.updateLastPosition();
       }
+
       for (let i = this.shape.start.row; i < this.shape.end.row; i++) {
         for (let j = this.shape.start.col; j < this.shape.end.col; j++) {
           if (this.shape.getByPosition(i, j) === 1)
